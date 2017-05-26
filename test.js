700336f6a8f0a40342130a3f149900280d755f68
Rishi Giri <rushforlinux@gmail.com>
2017-05-27 02:21:28 +0530

    udpated test

--- test.js
+++ test.js
@@ -1,12 +1,14 @@
-import childProcess from 'child_process';
+import childProces from 'child_process';
 import test from 'ava';
 
 test.cb(t => {
-	childProcess.execFile('./cli.js', ['-u'], {
-		cwd: __dirname
-	}, (err, stdout) => {
-		t.ifError(err);
-		t.true(stdout.trim().length === 0);
+	const cp = childProces.spawn('./cli.js', ['rushforlinux', '--id'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
 		t.end();
 	});
 });
+
